0ef2abc68320d79a7b391e2a9dacdd62b238dc5b
yashsb-07 <yashsbansode07@gmail.com>
2026-03-10 11:13:11 +0530

    added login retry system project

--- PYTHON/05_projects.py
+++ PYTHON/05_projects.py
@@ -1,22 +1,53 @@
-from random import randint
+#Guessing game
 
-random_num = randint(1, 10)
-user_num = 0
-counter = 0
+# from random import randint
 
-print("Guess the number between 1 to 10.")
+# random_num = randint(1, 10)
+# user_num = 0
+# counter = 0
 
-while user_num != random_num:
+# print("Guess the number between 1 to 10.")
 
-    user_num = int(input("\nGuess your number: "))
-    counter += 1
+# while user_num != random_num:
 
-    if user_num == random_num:
-        print("Congratulations ✨🎊 You won!")
-        print(f"Total attempts: {counter}")
+#     user_num = int(input("\nGuess your number: "))
+#     counter += 1
 
-    elif user_num > random_num:
-        print("Number is too high. Try smaller!")
+#     if user_num == random_num:
+#         print("Congratulations ✨🎊 You won!")
+#         print(f"Total attempts: {counter}")
+
+#     elif user_num > random_num:
+#         print("Number is too high. Try smaller!")
+
+#     else:
+#         print("Number is too small. Try larger!")
+
+#Login system with retry limit
+
+correct_username = "yashsb07"
+correct_password = "Yash@123"
+
+attempts = 0
+max_attempts = 3
+
+while attempts < max_attempts:
+
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    attempts += 1
+
+    if username == correct_username and password == correct_password:
+        print("Access Granted!")
+        break
 
     else:
-        print("Number is too small. Try larger!")
+        if attempts < max_attempts:
+            remaining_attempts = max_attempts - attempts
+            print("Access denied! Try again.")
+            print(f"Attempts left: {remaining_attempts}")
+        else:
+            print("Account locked.")
+        
+    
