53aefe133cffaff8c666d9cb8cd8720372be5fef
yashsb-07 <yashsbansode07@gmail.com>
2026-03-23 10:11:47 +0530

    Learn functions in python

---
+++ PYTHON/09_function_recursion.py
@@ -0,0 +1,23 @@
+#Functions
+
+def cal_sum(a, b):
+    sum = a + b
+    print(sum)
+
+cal_sum(5, 10)
+
+def cal_avg(a, b, c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+
+cal_avg(98, 78, 90)
+
+def printHello():
+    print("Hello")
+
+printHello()
+printHello()
+printHello()
+printHello()
+printHello()
