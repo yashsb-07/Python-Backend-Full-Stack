094e98d01f8df5c44e49e0191d226883b5576e63
yashsb-07 <yashsbansode07@gmail.com>
2026-03-24 11:35:01 +0530

    Learning JavaScript Fundamentals

---
+++ 03_JavaScript/app.js
@@ -0,0 +1,73 @@
+/* This file connected with D:\My-Projects\Python Full-Stack\01_HTML\05_html_js\index.html */
+
+/* Console Use */
+
+// console.log("Hello World!")
+
+/* Variables */
+
+// let a = 5
+// let name = "Yash"
+// console.log(a, name)
+
+/* Template Literals */
+
+// let firstName = "Yash"
+// console.log(`Hello ${firstName}`)
+
+// /* Conditional Statements */
+
+// let age = 23
+// if (age > 18) {
+//     console.log("You can vote")
+// }
+
+/* Q1 */
+
+// let light = "Yellow"
+
+// if (light == "Red"){
+//     console.log("Stop")
+// }
+// if (light == "Yellow"){
+//     console.log("Wait")
+// }
+// if (light == "Green"){
+//     console.log("Go")
+// }
+
+/* Q2 */
+
+// let marks = 33
+
+// if (marks >= 35){
+//     console.log("Pass")
+// } else {
+//     console.log("Fail")
+// }
+
+/* Q3 */
+
+// let my_age = 2;
+
+// if (my_age > 18){
+//     console.log("You can vote")
+// } else if (my_age < 18) {
+//     console.log("You cannnot vote")
+// } 
+
+/* Nested if-else */
+
+let student_marks = 79
+
+if (student_marks >= 35){
+    console.log("Pass")
+    
+    if (student_marks >= 80){
+        console.log("Grade: A")
+    } else {
+        console.log("Grade: B")
+    }
+} else {
+    console.log("Better luck next time. ")
+}
