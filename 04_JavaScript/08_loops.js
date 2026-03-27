7b267520feae505202575dc7bfc72d4ad63bc1a8
yashsb-07 <yashsbansode07@gmail.com>
2026-03-27 16:06:01 +0530

    Learned loops in javascript

---
+++ 04_JavaScript/08_loops.js
@@ -0,0 +1,58 @@
+/* For Loop */
+
+/* Print 1 to 20 */
+
+// for (let i = 2; i<= 20; i=i+2){
+//     console.log(i)
+// }
+
+/* While Loops */
+
+// i = 0
+
+// while (i<=10){
+//     console.log(i)
+//     i = i+2;
+// }
+
+/* Guessing Game */
+
+// let fav_movie = "knph";
+
+// let guess = prompt("Enter my guess movie: ");
+
+// while ((guess != fav_movie) && (guess != "quit")){
+//     guess = prompt("Wrong guess. Please try again!")
+// }
+
+// if (guess == fav_movie){
+//     console.log("congratulations..!")
+// } else {
+//     console.log("You quit. ")
+// }
+
+/* Break Keyword */
+
+// i = 1
+
+// while (i<=10){
+    // if(i == 3){
+    //     break
+    // }
+
+//     console.log(i)
+// }
+
+// let heros = [["spiderman", "ironman", "captain america"], ["wonder women", "flash", "superman"]]
+
+// for (let i = 0; i < heros.length; i++){
+//     console.log(i, heros[i], heros[i].length);
+//     for(let j = 0; j<heros[i].length; j++){
+//         console.log(`j=${j}, ${heros[i][j]}`);
+//     }
+// }
+
+
+
+
+
