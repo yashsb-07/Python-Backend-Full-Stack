f0ef39d5e4f0052cf818160cf5d322825495f478
yashsb-07 <yashsbansode07@gmail.com>
2026-04-10 16:49:53 +0530

    Learned functions and create rolldice game fumction

---
+++ 04_JavaScript/10_functions.js
@@ -0,0 +1,15 @@
+function printHello() {
+    console.log("Hello Yash! How are you?")
+};
+
+function rollDice() {
+    let dice = Math.floor(Math.random() * 6 ) + 1;
+    console.log(dice)
+}
+
+rollDice()
+rollDice()
+rollDice()
+rollDice()
+rollDice()
+rollDice()
