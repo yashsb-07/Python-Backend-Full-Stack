88d4eff7cd016b333abdf6c255272d057f5809bd
yashsb-07 <yashsbansode07@gmail.com>
2026-04-12 21:15:53 +0530

    Added password checker program

--- 04_JavaScript/08_practice_all_above.js
+++ 04_JavaScript/08_practice_all_above.js
@@ -401,20 +401,59 @@
 
 /* 2. Number Guessing Game */
 
-let randomNumber = Math.floor(Math.random() * 10) + 1;
+// let randomNumber = Math.floor(Math.random() * 10) + 1;
 
-let guess;
+// let guess;
 
-while (guess !== randomNumber) {
-  guess = Number(prompt("Enter a number between 1 and 10:"));
+// while (guess !== randomNumber) {
+//   guess = Number(prompt("Enter a number between 1 and 10:"));
 
-  if (guess > randomNumber) {
-    console.log("Too high! Try again.");
-  } else if (guess < randomNumber) {
-    console.log("Too low! Try again.");
-  } else if (guess === randomNumber) {
-    console.log("Correct! You guessed it 🎉");
+//   if (guess > randomNumber) {
+//     console.log("Too high! Try again.");
+//   } else if (guess < randomNumber) {
+//     console.log("Too low! Try again.");
+//   } else if (guess === randomNumber) {
+//     console.log("Correct! You guessed it 🎉");
+//   } else {
+//     console.log("Invalid input!");
+//   }
+// }
+
+/* 3. Password Checker */
+
+function checkPassword(password) {
+  
+  if (password.length < 8) {
+    console.log("Invalid: Password must be at least 8 characters long");
+    return;
+  }
+
+  let hasNumber = false;
+  let hasSpecialChar = false;
+
+  for (let i = 0; i < password.length; i++) {
+    let char = password[i];
+
+    if (char >= '0' && char <= '9') {
+      hasNumber = true;
+    }
+
+    else if (
+      (char >= '!' && char <= '/') ||
+      (char >= ':' && char <= '@') ||
+      (char >= '[' && char <= '`') ||
+      (char >= '{' && char <= '~')
+    ) {
+      hasSpecialChar = true;
+    }
+  }
+
+if (hasNumber && hasSpecialChar) {
+    console.log("Valid Password");
   } else {
-    console.log("Invalid input!");
+    console.log("Invalid: Must contain at least one number and one special character");
   }
 }
+
+let password = prompt("Enter your password:");
+checkPassword(password);
