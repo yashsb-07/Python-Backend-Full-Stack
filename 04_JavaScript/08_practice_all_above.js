3332a4ef1633e2363ba0762aedd19dce156b1bec
yashsb-07 <yashsbansode07@gmail.com>
2026-04-12 19:32:32 +0530

    Added FizzBuzz code

--- 04_JavaScript/08_practice_all_above.js
+++ 04_JavaScript/08_practice_all_above.js
@@ -381,3 +381,21 @@
 // }
 
 // console.log(unique);
+
+
+/* Level 3 */
+
+/* 1. FizzBuzz */
+
+for(let i=1; i<=100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
+    } else if(i % 3 === 0){
+        console.log("Fizz");
+    } else if(i % 5 === 0){
+        console.log("Buzz");
+    } else{
+        console.log(i);
+    }
+}
+
