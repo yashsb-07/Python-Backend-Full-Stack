eed4f0725ba9a06202577867f4f8f61f0e290b9b
yashsb-07 <yashsbansode07@gmail.com>
2026-04-13 11:30:58 +0530

    added quiz game

--- 04_JavaScript/11_projects.js
+++ 04_JavaScript/11_projects.js
@@ -1,43 +1,85 @@
-let expenses = [];
+/* Expense Tracker */
 
-let count = Number(prompt("Enter number of expenses:"));
+// let expenses = [];
 
-if (isNaN(count) || count <= 0) {
-    console.log("Invalid number of expenses");
-} else {
+// let count = Number(prompt("Enter number of expenses:"));
 
-    for (let i = 1; i <= count; i++) {
+// if (isNaN(count) || count <= 0) {
+//     console.log("Invalid number of expenses");
+// } else {
 
-        let name = prompt(`Enter expense ${i} name:`);
-        let amount = Number(prompt(`Enter amount for ${name}:`));
+//     for (let i = 1; i <= count; i++) {
 
-        if (isNaN(amount) || amount <= 0) {
-            console.log("Invalid amount, skipping...");
+//         let name = prompt(`Enter expense ${i} name:`);
+//         let amount = Number(prompt(`Enter amount for ${name}:`));
+
+//         if (isNaN(amount) || amount <= 0) {
+//             console.log("Invalid amount, skipping...");
+//             continue;
+//         }
+
+//         expenses.push(amount);
+//     }
+
+//     function calculateTotal(arr) {
+//         let total = 0;
+//         for (let i = 0; i < arr.length; i++) {
+//             total += arr[i];
+//         }
+//         return total;
+//     }
+
+//     function findMax(arr) {
+//         let max = arr[0];
+//         for (let i = 0; i < arr.length; i++) {
+//             if (arr[i] > max) {
+//                 max = arr[i];
+//             }
+//         }
+//         return max;
+//     }
+
+//     console.log("Expenses:", expenses);
+//     console.log("Total Expense:", calculateTotal(expenses));
+//     console.log("Highest Expense:", findMax(expenses));
+// }
+
+/* Quiz Game */
+
+let questions = ["What is 2 + 2?", "Which language runs in browser", "What is capital of India?"];
+
+let options = [["1. 3", "2. 4", "3. 5"], ["1. Python", "2. Java", "3. JavaScript"], ["1. Mumbai", "2. Delhi", "3. Pune"]];
+
+let answers = [2, 3, 2];
+
+let score = 0;
+
+function startQuiz(){
+    for (let i=0; i<questions.length; i++){
+        console.log(`\nQ${i + 1}: ${questions[i]}`);
+
+        for (let j=0; j<options[i].length; j++){
+            console.log(options[i][j]);
+        }
+
+        let useranswer = Number(prompt("Enter your answers (1-3): "));
+
+        if (useranswer < 1 || useranswer > 3 || isNaN(useranswer)){
+            console.log("Invalid input.");
             continue;
         }
 
-        expenses.push(amount);
-    }
-
-    function calculateTotal(arr) {
-        let total = 0;
-        for (let i = 0; i < arr.length; i++) {
-            total += arr[i];
+        if (useranswer === answers[i]){
+            console.log("Correct");
+            score++;
+        } else {
+            console.log("Wrong")
         }
-        return total;
     }
 
-    function findMax(arr) {
-        let max = arr[0];
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
+    console.log("\nQuiz Finished");
+    console.log(`Your score ${score}/${questions.length}`);
 
-    console.log("Expenses:", expenses);
-    console.log("Total Expense:", calculateTotal(expenses));
-    console.log("Highest Expense:", findMax(expenses));
 }
+
+startQuiz()
