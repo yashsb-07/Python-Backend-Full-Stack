33660ae126d9fe228dcad02061b6397689d3b313
yashsb-07 <yashsbansode07@gmail.com>
2026-04-16 09:37:22 +0530

    Learned getElementById, getElementsByClassName, getElementsByTagName

--- 04_JavaScript/12_dom.js
+++ 04_JavaScript/12_dom.js
@@ -2,3 +2,13 @@ console.dir(document.body)
 console.log(document.body)
 
 document.body.style.background = "red"
+
+/* DOM Manipulation */
+// let heading = document.getElementById("heading");
+// console.log(heading)
+
+// let headings = document.getElementsByClassName("heading")
+// console.log(headings)
+
+let parah = document.getElementsByTagName("p")
+console.log(parah)
