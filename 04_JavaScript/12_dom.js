2e68161c0b274be060c87e41c7510637c3c6eab0
yashsb-07 <yashsbansode07@gmail.com>
2026-04-16 10:09:35 +0530

    Learned properties

--- 04_JavaScript/12_dom.js
+++ 04_JavaScript/12_dom.js
@@ -1,14 +1,29 @@
-console.dir(document.body)
-console.log(document.body)
+// console.dir(document.body)
+// console.log(document.body)
 
-document.body.style.background = "red"
+// document.body.style.background = "red"
 
 /* DOM Manipulation */
+
 // let heading = document.getElementById("heading");
 // console.log(heading)
 
 // let headings = document.getElementsByClassName("heading")
 // console.log(headings)
 
-let parah = document.getElementsByTagName("p")
-console.log(parah)
+// let parah = document.getElementsByTagName("p")
+// console.log(parah)
+
+// let el = document.querySelector("p"); // return first element in document
+// console.dir(el);
+
+// let elm = document.querySelectorAll("p"); // return all elements in document
+// console.dir(elm)
+
+// let id = document.querySelector("#heading");
+// console.log(id)
+
+// let div = document.querySelector("div");
+// console.log(div.innerText); // return only visible text
+// console.log(div.innerHTML);
+// console.log(div.textContent); // return hidden elements also
