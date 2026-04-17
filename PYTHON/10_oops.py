1baf7fd1fca0891582ab80682279f74f37912462
yashsb-07 <yashsbansode07@gmail.com>
2026-04-17 14:24:12 +0530

    Learned __init__ function

--- PYTHON/10_oops.py
+++ PYTHON/10_oops.py
@@ -1,18 +1,29 @@
 # Classes & Objects
 
-class Student: # Creating class
-    name = "Yash"
+# class Student: # Creating class
+#     name = "Yash"
 
-s1 = Student() #Creating object
+# s1 = Student() #Creating object
+# print(s1.name)
+
+# class Car:
+#     color = "Black"
+#     brand = "BMW"
+
+# car = Car()
+# print(car.color)
+# print(car.brand)
+
+# __init__ Function or constructor
+
+class Student():
+    def __init__(self, fullName):
+        self.name = fullName
+        print("Adding new student in database")
+
+    
+s1 = Student("Yash Bansode")
 print(s1.name)
-
-class Car:
-    color = "Black"
-    brand = "BMW"
-
-car = Car()
-print(car.color)
-print(car.brand)
-
-
+s2 = Student("Sakshi Bansode")
+print(s2.name)
 
