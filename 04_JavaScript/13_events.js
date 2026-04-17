f69dd854171ad32c11b97fbffedd697f69ed5175
yashsb-07 <yashsbansode07@gmail.com>
2026-04-17 14:05:29 +0530

    learned events in javascript

--- 04_JavaScript/13_events.js
+++ 04_JavaScript/13_events.js
@@ -1,11 +1,50 @@
-let btn = document.querySelector("#btn");
+// let btn = document.querySelector("#btn");
 
-btn.onclick = (evt) => {
-    console.log("Button was clicked!");
-}
+// btn.onclick = (evt) => {
+//     console.log("Button was clicked!");
+// }
 
-let box = document.querySelector(".box");
+// let box = document.querySelector(".box");
 
-box.onmouseover = (evt ) => {
-    console.log("you are inside div.");
-}
+// box.onmouseover = (evt ) => {
+//     console.log("you are inside div.");
+// }
+
+/* EventListener */
+
+// btn.addEventListener("click", () =>{
+//     console.log("Button was clicked - handler 1");
+// })
+
+// btn.addEventListener("click", () =>{
+//     console.log("Button was clicked - handler 2");
+// })
+
+// const handler3 = () => {
+//     console.log("Button was clicked - handler 3");
+// }
+
+// btn.addEventListener("click", handler3);
+
+
+// btn.addEventListener("click", () =>{
+//     console.log("Button was clicked - handler 4");
+// })
+
+// btn.removeEventListener("click", handler3);
+
+let modeBtn = document.querySelector("#mode");
+
+let currentMode = "light-mode";
+
+modeBtn.addEventListener("click", () => {
+    if(currentMode === "light-mode") {
+        currentMode = "dark-mode";
+        document.querySelector("body").style.background = "black";
+    } else {
+        currentMode = "light-mode";
+        document.querySelector("body").style.background = "white";
+    }
+
+    console.log(currentMode);
+});
