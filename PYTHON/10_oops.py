a89ce64687d81b75033f1afc257ee2030bf4b4d1
yashsb-07 <yashsbansode07@gmail.com>
2026-04-17 14:21:02 +0530

    Learning objects and classes

---
+++ PYTHON/10_oops.py
@@ -0,0 +1,18 @@
+# Classes & Objects
+
+class Student: # Creating class
+    name = "Yash"
+
+s1 = Student() #Creating object
+print(s1.name)
+
+class Car:
+    color = "Black"
+    brand = "BMW"
+
+car = Car()
+print(car.color)
+print(car.brand)
+
+
+
