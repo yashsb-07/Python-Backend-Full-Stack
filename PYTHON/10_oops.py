4169f968331e57109c90012f0bf197d5d2b2f959
yashsb-07 <yashsbansode07@gmail.com>
2026-04-20 17:34:54 +0530

    Solved some questions

--- PYTHON/10_oops.py
+++ PYTHON/10_oops.py
@@ -293,7 +293,48 @@
 # num3 = num1 + num2
 # num3.showNumber()
         
-        
+#Question1    
+
+# class Circle():
+
+#     def __init__(self, radius):
+#         self.radius = radius
+    
+#     def area(self):
+#         return 3.14 * self.radius ** 2
+    
+#     def perimeter(self):
+#         return 2 * 3.14 * self.radius
+    
+# c1 = Circle(21)           
+# print(c1.area())
+# print(c1.perimeter())
+
+#Question2
+
+# class Employee:
+#     def __init__(self, role, dept, salary):
+#         self.role = role
+#         self.dept = dept
+#         self.salary = salary
+
+#     def showDetails(self):
+#         print("Role: ", self.role)
+#         print("Dept: ", self.dept)
+#         print("Salary: ", self.salary)
+
+ 
+# class Engineer(Employee):
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         super().__init__("SDE", "IT", "1,00,000")
 
         
 
+# emp = Employee("SDE", "IT", "1,00,000") 
+# emp.showDetails()
+
+# eng = Engineer("Yash", 22)
+# eng.showDetails()
+        
