8855f68cb23b663e11ddd2dc57f94f4674dc4979
yashsb-07 <yashsbansode07@gmail.com>
2026-04-20 11:43:35 +0530

    Learned polymorphism

--- PYTHON/10_oops.py
+++ PYTHON/10_oops.py
@@ -269,6 +269,31 @@
 # s1.phy = 86
 # print(s1.percentage)
         
+#Polymorphism
+
+# class Complex:
+#     def __init__(self, real, img):
+#         self.real = real
+#         self.img = img
+
+#     def showNumber(self):
+#         print(self.real, "i +", self.img, "j")
+
+#     def __add__(self, num2):
+#         newReal = self.real + num2.real
+#         newImg = self.img + num2.img
+#         return Complex(newReal, newImg)
+
+# num1 = Complex(1, 2)
+# num1.showNumber()
+
+# num2 = Complex(3, 4)
+# num2.showNumber()
+
+# num3 = num1 + num2
+# num3.showNumber()
+        
+        
 
         
 
