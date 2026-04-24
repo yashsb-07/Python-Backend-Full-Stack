545229978c3dbec79e706c9b40e9edf6cf7ed289
yashsb-07 <yashsbansode07@gmail.com>
2026-04-24 16:49:10 +0530

    Learning Fetch API

--- 04_JavaScript/14_oops.js
+++ 04_JavaScript/14_oops.js
@@ -23,21 +23,50 @@ class ToyotaCar{
 
 /* Inheritance */
 
-class Person {
-    eat(){
-        console.log("Eat");
-    }
+// class Person {
+//     eat(){
+//         console.log("Eat");
+//     }
 
-    sleep(){
-        console.log("Sleep")
-    }
-}
+//     sleep(){
+//         console.log("Sleep")
+//     }
+// }
 
-class Engineer extends Person {
-    work(){
-        console.log("Solve problems, build something.")
-    }
-}
+// class Engineer extends Person {
+//     work(){
+//         console.log("Solve problems, build something.")
+//     }
+// }
 
-let yash = new Engineer();
-console.log(yash.eat());
+// let yash = new Engineer();
+// console.log(yash.eat());
+
+/* API */
+
+const URI = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
+
+const getFacts = async () => {
+    console.log("Getting data..");
+    let responce = await fetch(URI);
+    console.log(responce);
+    let data = await responce.json();
+    factPara.innerText = data[2].text;
+};
+
+// Using Promice Chain
+
+// function getFacts() {
+//     fetch(URI)
+//         .then((responce) => {
+//             return responce.json();
+//         })
+//         .then((data) => {
+//             console.log(data);
+//             factPara.innerText = data[2].text;
+//         });
+// }
+
+btn.addEventListener("click", getFacts);
