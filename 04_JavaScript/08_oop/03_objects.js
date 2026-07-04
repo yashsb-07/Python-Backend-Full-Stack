469272afdf5f792add3b7cee878c6adc21b1bb4f
yashsb-07 <yashsbansode07@gmail.com>
2026-07-04 11:14:07 +0530

    learned new keyword in dept

---
+++ 04_JavaScript/08_oop/03_objects.js
@@ -0,0 +1,28 @@
+function multiplyBy5(num){
+    return num*5
+}
+
+multiplyBy5.power = 2
+
+console.log(multiplyBy5(5));
+console.log(multiplyBy5.power);
+console.log(multiplyBy5.prototype);
+
+
+function createUser(username, score){
+    this.username = username;
+    this.score = score
+}
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`Price is ${this.score}`);
+}
+
+const chai = new createUser("Chai", 25);
+const tea =  createUser("Tea", 250)
+
+chai.printMe()
