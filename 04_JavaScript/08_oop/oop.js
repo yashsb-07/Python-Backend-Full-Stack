5fb7806512ede633d4b39542c9c3933b34666bb3
yashsb-07 <yashsbansode07@gmail.com>
2026-07-04 10:47:30 +0530

    Learn New keyword

--- 04_JavaScript/08_oop/oop.js
+++ 04_JavaScript/08_oop/oop.js
@@ -1,14 +1,27 @@
-const user = {
-    username: "yash",
-    loginCount: 7,
-    signIn: true,
+// const user = {
+//     username: "yash",
+//     loginCount: 7,
+//     signIn: true,
 
-    getUserDetials: function() {
-        console.log(`Username: ${this.username}`);
+//     getUserDetials: function() {
+//         console.log(`Username: ${this.username}`);
         
-        console.log("Got user details from database");
-    }
-}
+//         console.log("Got user details from database");
+//     }
+// }
 
 // console.log(user.username);
-console.log(user.getUserDetials());
+// console.log(user.getUserDetials());
+
+function User(username, loginCount, signIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.signIn = signIn
+
+    return this
+}
+
+const userOne = new User("Yash", 7, true)
+const userTwo = new User("Sakshi", 12, false)
+console.log(userOne);
+console.log(userTwo);
